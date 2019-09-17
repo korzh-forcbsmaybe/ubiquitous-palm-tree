d0ca6cb5be14c9bfeb4966086dddea85919fe286
Sasha <pdenna13@gmail.com>
2019-09-17 13:10:00 +0300

    new version of our project

---
+++ src/main/java/pac/MyClass.java
@@ -0,0 +1,17 @@
+package pac;
+
+public class MyClass {
+    private int field;
+
+    public MyClass(int field) {
+        this.field = field;
+    }
+
+    public int getField() {
+        return field;
+    }
+
+    public void setField(int field) {
+        this.field = field;
+    }
+}
