2de10ce342ac1c58409e4f4d9deb5a3ac890b894
Sasha <pdenna13@gmail.com>
2019-09-17 13:29:25 +0300

    add NewClass

---
+++ src/main/java/pac/NewClass.java
@@ -0,0 +1,13 @@
+package pac;
+
+public class NewClass {
+    private String field;
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+}
